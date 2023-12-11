a57a69a06a4df97fd5095f5f082cfb0fe3289410
Soveliy <34750690+Soveliy@users.noreply.github.com>
2023-12-11 20:35:07 +0300

    FirstLesson

---
+++ app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <div class="card__title">Заголовок в app</div>
+
+  <Card title="Рубашка 1" :price="30000 + 500" :is-added="true">
+    <Card title="Рубашка 1" :price="30000 + 500" :is-added="true"> </Card>
+  </Card>
+
+  <Card title="Рубашка 1" :price="30000 + 500" :is-added="true"> </Card>
+  <Card title="Рубашка 1" :price="30000 + 500" :is-added="true"> </Card>
+</template>
+
+<style>
+body {
+  background-color: #000;
+  color: #fff;
+}
+</style>

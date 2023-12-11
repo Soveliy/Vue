760fc3fc1878d71a0063dd59499770c40b5009c5
Soveliy <34750690+Soveliy@users.noreply.github.com>
2023-12-11 21:05:55 +0300

    add sprites

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,11 +4,16 @@ import Card from './components/Card.vue'
 
 <template>
   <div class="card__title">Заголовок в app</div>
-
+  <svg>
+    <use href="./assets/sprite.svg#icon-fav"></use>
+  </svg>
+  <svg>
+    <use href="./assets/sprite.svg#icon-lk"></use>
+  </svg>
   <Card title="Рубашка 1" :price="30000 + 500" :is-added="true">
-    <Card title="Рубашка 1" :price="30000 + 500" :is-added="true"> </Card>
+    <button>Добавить в корзину</button>
   </Card>
-
+  <Card title="Рубашка 1" :price="30000 + 500" :is-added="true"> </Card>
   <Card title="Рубашка 1" :price="30000 + 500" :is-added="true"> </Card>
   <Card title="Рубашка 1" :price="30000 + 500" :is-added="true"> </Card>
 </template>
